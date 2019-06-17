f86c24a411d4a26fd189f8df9983bbb505a03219
HaiQue <43703237+HaiQue@users.noreply.github.com>
2019-06-17 15:17:38 +0300

    Create Checkerboard.h

---
+++ Checkerboard.h
@@ -0,0 +1,31 @@
+#pragma once
+#ifndef CHECKERBOARD_H
+#define CHECKERBOARD_H
+
+#include <string>
+
+using namespace std;
+
+class CheckerBoard {
+public:
+	void displayTitle(void);
+	void displayScore(void);
+	void updateScore(string& player, int jumpOption);
+	void initBoard(void);
+	void displayBoard(void);
+	string& getSquare(int row, int col);
+	void updateBoard(int row, int col, string& square);
+	void clearSquare(int row, int col);
+	void checkForKing(void);
+	bool checkWinner(void);
+	const int getRPieces(void);
+	const int getBPieces(void);
+
+private:
+	string m_board[8][8];
+	int m_rPieces;
+	int m_bPieces;
+};
+
+
+#endif
